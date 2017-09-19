72c02984772609af901955e1310a9aabd66f1b00
johanmorales <jhellmetal2000@gmail.com>
2017-09-19 14:37:58 -0500

    Se añade el reto 01

---
+++ reto01/js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+
+    $("#dragDropTest").dragDropTest({
+    	header: 'La siguiente actividad le ayudará a reforzar sus conocimientos con respecto al origen y el significado de la epistemología. <br> <br> Haga clic sostenido para relacionar el concepto con su definición correcta. Después de realizado el ejercicio haga clic en el botón “Verificar”.',
+    	contButtonValidate: 'Verificar',
+    	genHeight: '65px',
+    	definitions: {
+			1: "Intenta dar respuestas acerca de los principales supuestos relacionados con el conocimiento que se genera en el ámbito científico.",
+			2: "Inscribe todo propósito del conocer y hacia la cual convergen todas las explicaciones.",
+			3: "Matriz de pensamiento que orienta la generación de conocimiento, desde los supuestos acerca del saber, su concepto, su propósito y sus criterios de validación.",
+			4: "Visión parcial de una realidad, a partir de un punto de vista o perspectiva, que hace énfasis en ciertos aspectos de lo observado, pero omite otros que no pueden ser vistos desde la perspectiva en cuestión."
+		},
+		//se definen los drags a los drops
+		drags: [
+			{				
+				content: "Paradigma",
+				goesWith: 3 //para drop 1 drag numero 3
+			},
+			{				
+				content: "Modelo epistémico",
+				goesWith: 4 //para drop 2 numero del drag numero 4
+			},
+			{			
+				content: "Epistemología",
+				goesWith: 2 //para drop 3 numero del drag numero 2
+			},
+			{				
+				content: "Filosofía",
+				goesWith: 1 //para drop 4 numero del drag numero 1
+			}
+		],
+		/*start: function(){
+			alert("Inicia el test!")
+		}*/
+    });
+})

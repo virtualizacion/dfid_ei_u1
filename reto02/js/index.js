bee3b16f877dca068bbcd05036565c9b5d060e9c
krapu1 <krapu1@hotmail.com>
2017-09-25 19:14:27 -0500

    Desarrollo del reto 2.

--- reto02/js/index.js
+++ reto02/js/index.js
@@ -3,32 +3,37 @@ $(function(){
 	dr.showInst();
 
     $("#dragDropTest").dragDropTest({
-    	header: 'La siguiente actividad le ayudará a reforzar sus conocimientos con respecto al origen y el significado de la epistemología. Haga clic sostenido para relacionar el concepto con su definición correcta. Después de realizado el ejercicio haga clic en el botón “Verificar”.',
+    	header: 'A continuación encontrará algunos de los modelos epistémicos tratados anteriormente, para que compruebe su proceso de estudio. Arrastre la respuesta al modelo que le corresponde.',
     	contButtonValidate: 'Verificar',
-    	genHeight: '58px',
+    	genHeight: '64px',
     	classOffsetColGen: "col-xs-offset-1",
     	definitions: {
-			1: "Intenta dar respuestas acerca de los principales supuestos relacionados con el conocimiento que se genera en el ámbito científico.",
-			2: "Inscribe todo propósito del conocer y hacia la cual convergen todas las explicaciones.",
-			3: "Matriz de pensamiento que orienta la generación de conocimiento, desde los supuestos acerca del saber, su concepto, su propósito y sus criterios de validación.",
-			4: "Visión parcial de una realidad, a partir de un punto de vista o perspectiva, que hace énfasis en ciertos aspectos de lo observado, pero omite otros que no pueden ser vistos desde la perspectiva en cuestión."
+			1: "Su concepto básico está definido un conjunto de elementos que conforman una unidad, de modo tal que hay una ley que determina la composición interna de dicha unidad, y la modificación de una de las partes implica la modificación de las otras.",
+			2: "Centra la importancia de los hechos por encima de las ideas, en donde la experimentación en vez de los desarrollos fundamentalmente teóricos.",
+			3: "Una de sus planteamientos principales está fundamentado en el cambio como un proceso que obedece a contradicciones y busca que el estudio de la sociedad se realice partir de lo real y no de las ideas acerca de lo real.",
+			4: "Para este modelo, las teorías son relevantes solo si guían la práctica y tienen la capacidad de generar resultados deseados para resolver los dilemas humanos y sociales.",
+			5: "Considera que los objetos del conocimiento son definidos por operaciones activas, y el conocimiento es un producto de la acción humana. No establece una dicotomía entre teoría y práctica."		
 		},
 		//se definen los drags a los drops
 		drags: [
 			{				
-				content: "Paradigma",
+				content: "Pragmatismo",
 				goesWith: 3 //para drop 1 drag numero 3
 			},
 			{				
-				content: "Modelo epistémico",
+				content: "Materialismo dialéctico",
 				goesWith: 4 //para drop 2 numero del drag numero 4
 			},
 			{			
-				content: "Epistemología",
+				content: "Estructuralismo",
 				goesWith: 2 //para drop 3 numero del drag numero 2
 			},
 			{				
-				content: "Filosofía",
+				content: "Positivismo",
+				goesWith: 5 //para drop 4 numero del drag numero 1
+			},
+			{				
+				content: "Pragmatismo sociologista",
 				goesWith: 1 //para drop 4 numero del drag numero 1
 			}
 		],
